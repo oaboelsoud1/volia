0c7cdc0314f335cbb78d143daf34e652227b016d
omaraboelsoud <oaboelsoud@sympl.ai>
2021-11-24 23:18:35 +0200

    cleaning

--- src/main/java/com/volia/notificationservice/model/dto/UserCreationRequestDto.java
+++ src/main/java/com/volia/notificationservice/model/dto/UserCreationRequestDto.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
